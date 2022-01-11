168e4517e0f797206d45f6ebf2025c62924d3633
ethanbond64 <ethanbond39@gmail.com>
2022-01-10 19:02:52 -0700

    api stubs

--- Effect_Notebooks/codebase/server/batch_client.py
+++ Effect_Notebooks/codebase/server/batch_client.py
@@ -11,7 +11,51 @@ app = Flask(__name__, template_folder=template_dir)
 @app.route("/")
 def entry_point():
     return render_template("helloworld.html")
-    # return "Hello World!"
+
+
+# BATCH ROUTES
+@app.route("/batch")
+def create_new_batch():
+
+    # Save 3 csvs
+    # og
+    # sample start
+    # sample end
+
+    # create ipynb
+    # fill in the fields
+    # give the client the name of the file
+
+    return "TODO: supply the data needed to create batch tasks"
+
+
+@app.route("/validate")
+def validate_task_submission():
+
+    # exec the method on sample start
+    # confirm output is equal to sample end
+
+    return "TODO: run a submitted method on the sample data"
+
+
+@app.route("/use")
+def use_task_submission():
+
+    # exec the method on og
+    # provide link to download the file (just an href to the literal filepath)
+
+    return "TODO: run a submitted method on the original data"
+
+
+# TEMPLATE ROUTES
+
+
+@app.route("/method")
+def get_method_def():
+
+    # given filename use the method to get the definition, return a big string
+
+    return "TODO: get the method out of a given file"
 
 
 if __name__ == "__main__":
