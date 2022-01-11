d50f6706070a001dee76c42dd203b5cbc7ab5e21
vibelyze <fabriziasantelia@gmail.com>
2022-01-11 15:17:27 +0100

    VIBELYZE

---
+++ vibelyze/codebase/README.md
@@ -0,0 +1,14 @@
+<p align="center"><img src="assets/img/logo/Logo-12.png" width="200px"></p>
+<p align="center">The Perceptive Essence of Music</p>
+
+Vibelyze is an app that investigates music nuance. Vibelyze obtains information suitable for training your music-related machine algorithm or designing your DJ setlist by handing songs to thousands of Effect Network workers.
+
+Video: [https://www.youtube.com/watch?v=O6vbbiE918Y](https://www.youtube.com/watch?v=O6vbbiE918Y)
+
+
+## Running the dApp
+
+```bash
+$ npm install
+$ npm run dev
+```
---
+++ vibelyze/codebase/pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <img src="../assets/img/logo/Logo-wit.png" class="mt-6 pt-6" style="width: 200px; margin: 0 auto; display: block;">
+    <div class="container mt-5 block p-6">
+        <div class="has-text-centered">
+          <p v-if="!account" class="mb-6">
+            <span class="title">Get started with VIBELYZE!</span><br>
+            Vibelyze is an app that investigates music nuance. Vibelyze obtains information suitable for training your music-related machine algorithm or designing your DJ setlist by handing songs to thousands of Effect workers.
+          </p>
+        </div>
+      <login v-if="!account" @account="setAccount"></login>
+      <search :effectsdk="effectsdk" :account="account" :newcampaign="newcampaign" v-if="account"></search>
+    </div>
+
+    <div class="mb-6 has-text-centered" v-if="account">
+      <p class="has-text-success" style="font-size: 18px;">Connected Effect Account: {{account.accountName}}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Login from '../components/Login.vue'
+import Search from '../components/Search.vue'
+import CreateCampaign from '../components/CreateCampaign.vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      account: null,
+      effectsdk: null,
+      search: false,
+      newcampaign: null
+    }
+  },
+  components: { Login, Search, CreateCampaign },
+  methods: {
+    setAccount (account, sdk) {
+      this.effectsdk = sdk
+      this.account = account
+    }
+  }
+
+})
+</script>
